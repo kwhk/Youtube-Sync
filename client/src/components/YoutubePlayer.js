4d047422edf174ef442379d8690a904f895c0fdd
Kevin Kim <kwkim@student.unimelb.edu.au>
2020-10-04 00:02:32 +0800

    :sparkles: finish basic front end youtube player

--- client/src/components/YoutubePlayer.js
+++ client/src/components/YoutubePlayer.js
@@ -2,49 +2,78 @@ import React, { Component, useState, useEffect } from 'react';
 import socketIo from "socket.io-client";
 
 import Youtube from 'react-youtube';
+import './YoutubePlayer.css';
 
-export default function YoutubePlayer() {
-  const opts = {
-    height: '390',
-    width: '640',
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      controls: 0,
-      disablekb: 0,
-      modestbranding: 1
-    },
+export default class YoutubePlayer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      player: null,
+      currTime: 0,
+      socket: null
+    }
+
+    this.opts = {
+      height: '390',
+      width: '640',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        controls: 0,
+        disablekb: 0,
+        modestbranding: 1
+      },
+    }
   }
 
-  const [player, setPlayer] = useState(null);
+  componentDidMount() {
+    this.setState({socket: socketIo(process.env.REACT_APP_API_HOST)}, () => {
+      console.log('socket connected');
+    });
+  }
 
-  const play = () => {
-    console.log('playing');
-    player.playVideo();
+  componentWillUnmount() {
+    this.state.socket.disconnect();
   }
   
-  const pause = () => {
+  play = () => {
+    console.log('playing');
+    this.state.player.playVideo();
+  }
+  
+  pause = () => {
     console.log('pause');
-    player.pauseVideo();
+    this.state.player.pauseVideo();
   }
 
-  const onReady = (e) => {
+  onReady = (e) => {
     console.log('onReady...');
-    setPlayer(e.target);
+    this.setState({player: e.target});
   }
 
-  useEffect(() => {
-    console.log('socket connected');
-    const socket = socketIo(process.env.REACT_APP_API_HOST);
-    return () => socket.disconnect();
-  });
+  changeTime = (e) => {
+    this.setState({currTime: e.target.value});
+  }
 
-  return (
-    <div>
-      <Youtube videoId="D1PvIWdJ8xo" opts={opts} onReady={onReady}/>
+  seek = (e) => {
+    console.log(e.target.value);
+  }
+
+  render() {
+    return (
       <div>
-        <button onClick={play}>play</button>
-        <button onClick={pause}>pause</button>
+        <Youtube videoId="D1PvIWdJ8xo" opts={this.opts} onReady={this.onReady}/>
+        <div className="d-flex flex-row justify-content-center width-100" style={{marginTop: "5px"}}>
+          <div>
+            <button onClick={this.play}>play</button>
+            <button onClick={this.pause}>pause</button>
+          </div>
+          <div>
+            <input id="audio-progress-bar" type="range" name="video-seek" min="0" max="100" value={this.state.currTime} onInput={this.changeTime} onMouseUp={this.seek} step="0.5"/>
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+
 }
