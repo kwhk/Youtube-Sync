54f17de53042ed1f39152dbb252d745312ded4b8
Kevin Kim <kwkim@student.unimelb.edu.au>
2020-10-05 22:04:16 +0800

    add broadcast emit event for all player controls

--- api/server/bin/socket.js
+++ api/server/bin/socket.js
@@ -3,9 +3,24 @@ sockets.init = function(server) {
   const io = require('socket.io')(server);
   io.on('connection', (socket) => {
     console.log('a user connected');
+
     socket.on('disconnect', () => {
       console.log('user disconnected');
-    })
+    });
+
+    socket.on('seekTo', sec => {
+      socket.broadcast.emit('seekTo', sec);
+    });
+
+    socket.on('pause', () => {
+      console.log('Broadcast: pause');
+      socket.broadcast.emit('pause');
+    });
+
+    socket.on('play', () => {
+      console.log('Broadcast: play');
+      socket.broadcast.emit('play');
+    });
   });
 }
 
