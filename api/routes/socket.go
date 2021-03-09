8c34800dcb7a298d1d43beada8088e2352105301
kwhk <kevin@fairvine.com.au-at-254115301960>
2021-03-09 17:49:18 +0800

    add worker pool

--- api/routes/socket.go
+++ api/routes/socket.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 	ws "github.com/kwhk/sync/api/web/websocket"
 	"github.com/kwhk/sync/api/repository"
+	wp "github.com/kwhk/sync/api/utils/workerPool"
 )
 
 // SocketRouter for websocket endpoints
 func SocketRouter(userRepo *repository.UserRepository, roomRepo *repository.RoomRepository) http.Handler {
 	wsServer := ws.NewWebsocketServer(roomRepo, userRepo)
+	msgWorkerPool := wp.NewPool()
 	go wsServer.Run()
 
 	r := chi.NewRouter()
