6ebdb149affc85087c596d11bc2d338c8ad4ea85
Kevin Kim <kwkim@student.unimelb.edu.au>
2021-04-19 18:05:37 +0800

    move socket context provider and pass in setSocket and add socket routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,66 +1,38 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import {
 	BrowserRouter as Router,
 	Switch,
 	Route
 } from 'react-router-dom'
 import './App.css';
-import SocketContext from './context/socket'
 import HomePage from './features/home/HomePage'
-import Socket from './api/socket'
-import Room from './features/room/Room'
-import useScript from './utils/useScript'
+import useScript from './hooks/useScript'
 import Navbar from './features/navbar/Navbar'
-import Ping from './api/ping'
+import SocketContext from './context/socket'
+import SocketRoute from './components/SocketRoute';
+import Room from './features/room/Room';
+import CreateRoom from './features/room/CreateRoom';
 
 function App() {
 	const [socket, setSocket] = useState(null)
-	// const history = createBrowserHistory()
-	
 	useScript('https://unpkg.com/ionicons@5.4.0/dist/ionicons.js')
-	useEffect(() => {
-		async function connect() {
-			const s = new Socket()
-			await s.connect()
-			new Ping(s)
-			setSocket(s)
-		}
 
-		connect()
-
-		return async function closeSocket() {
-			if (socket != null) {
-				console.log(socket)
-				await socket.disconnect()
-			}
-		}
-	}, []);
-
-
-	if (socket != null) {
-		return (
-			<SocketContext.Provider value={socket}>
-				<Router>
-					<div className="w-full">
-						<Navbar/>
-					</div>
-					<Switch>
-						<Route exact path="/">
-							<HomePage />
-						</Route>
-						{/* <Route exact path="/room/:id" render={(props) => <Room {...props}/>}/> */}
-						<Route exact path="/room/:id">
-							<Room />
-						</Route>
-					</Switch>
-				</Router>
-			</SocketContext.Provider>
-		);
-	} else {
-		return (
-			<div>Not connected</div>
-		)
-	}
+	return (
+		<SocketContext.Provider value={{socket, setSocket}}>
+			<Router>
+				<div className="w-full">
+					<Navbar/>
+				</div>
+				<Switch>
+					<Route exact path="/">
+						<HomePage />
+					</Route>
+					<SocketRoute component={CreateRoom} exact path="/room/create"/>
+					<SocketRoute component={Room} exact path="/room/:id"/>
+				</Switch>
+			</Router>
+		</SocketContext.Provider>
+	);
 }
 
 export default App;
