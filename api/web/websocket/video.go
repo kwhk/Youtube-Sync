ebf7ff6299b2211ee1a3a0e64299039df36d531c
Kevin Kim <kwkim@student.unimelb.edu.au>
2021-04-19 17:52:44 +0800

    implement Encodable interface to Video

--- api/web/websocket/video.go
+++ api/web/websocket/video.go
@@ -10,11 +10,9 @@ type Video struct {
 	URL string `json:"url"`
 	// Duration of video in ms.
 	Duration int64 `json:"duration"`
-	// Status to notify joining users playback state.
-	IsPlaying bool `json:"isPlaying"`
 }
 
-func (v Video) encode() []byte {
+func (v Video) Encode() []byte {
 	json, err := json.Marshal(v)
 	if err != nil {
 		log.Println(err)
@@ -30,8 +28,4 @@ func decodeVideo(p []byte) Video {
 	}
 
 	return video
-}
-
-func (v Video) GetEncoding() []byte {
-	return v.encode()
 }
