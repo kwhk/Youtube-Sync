615c10187beb1fa05bd1390b72efb4c09c9c8109
Kevin Kim <kwkim@student.unimelb.edu.au>
2021-04-19 18:00:56 +0800

    remove timer and add method to calculate time elapsed from redux store

--- client/src/features/sync/Synchronizer.js
+++ client/src/features/sync/Synchronizer.js
@@ -1,8 +1,8 @@
+import store from '../../store'
+
 export default class Synchronizer {
-        constructor(seekTo, player, timer, offset=0.5) {
-        // max time allowed from curr time to actual time before resyncing in seconds
+    constructor(seekTo, player, offset=0.5) {
         this.offset = offset;
-        this.timer = timer;
         this.seekTo = seekTo;
         this.player = player;
         this.interval = null;
@@ -11,17 +11,16 @@ export default class Synchronizer {
     start() {
         let self = this;
         let a = function() {
-            let elapsed = self.timer.elapsed() / 1000;
+            let elapsed = timerElapsed() / 1000;
             let currTime = self.player.getCurrentTime();
             let diff = Math.abs(elapsed - currTime);
-            console.log(`timer: ${elapsed.toFixed(2)}\nvideo: ${currTime.toFixed(2)}\ndiff:${diff.toFixed(2)}`)
+            // console.log(`timer: ${elapsed.toFixed(2)}\nvideo: ${currTime.toFixed(2)}\ndiff:${diff.toFixed(2)}`)
             if (diff > self.offset) {
-                console.log('NOT IN SYNC')
-                let elapsed = self.timer.elapsed()
+                // console.log('NOT IN SYNC')
+                let elapsed = timerElapsed()
                 let str = elapsed / 1000
-                console.log(`seekTo ${str.toFixed(2)}`)
+                // console.log(`seekTo ${str.toFixed(2)}`)
                 self.seekTo(elapsed);
-
             }
         }
 
@@ -33,5 +32,18 @@ export default class Synchronizer {
     stop() {
         clearInterval(this.interval);
         this.interval = null;
+    }
+}
+
+function timerElapsed() {
+    const state = store.getState()
+    const { start, progress, stop } = state.playerClock
+    const now = new Date()
+
+    if (stop) {
+        return progress
+    } else {
+        const elapsed = progress + (now.getTime() - start)
+        return elapsed
     }
 }
