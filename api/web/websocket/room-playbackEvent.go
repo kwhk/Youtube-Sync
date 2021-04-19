c09708a2d8bcf7d03a61099877ca167a2009188e
Kevin Kim <kwkim@student.unimelb.edu.au>
2021-04-19 17:50:40 +0800

    add player repo

--- api/web/websocket/room-playbackEvent.go
+++ api/web/websocket/room-playbackEvent.go
@@ -3,24 +3,27 @@ package websocket
 import (
 	"fmt"
 
+	"github.com/kwhk/sync/api/repository/redis"
 	"github.com/kwhk/sync/api/utils/clock"
 )
 
 type playback struct {
 	timestamp int64
-	currVideo *Video
 	clock *clock.Clock
 	message Message
 	action string
+	playerRepo redis.PlayerRepository
+	roomID string
 }
 
 func newPlayback(message Message, room *Room, action string) playback {
 	new := playback{
 		timestamp: int64(message.Data.(float64)),
-		currVideo: &room.Video.Curr,
 		clock: room.Clock,
 		message: message,
 		action: action,
+		playerRepo: room.wsServer.playerRepository,
+		roomID: room.ID,
 	}
 	return new
 }
@@ -41,20 +44,20 @@ func (p playback) handle() (Message, bool) {
 func (p playback) play() (Message, bool)  {
 	p.clock.SeekTo(p.timestamp).Play()
 	fmt.Printf("Play(), seconds elapsed: %2f\n", float64(p.clock.Elapsed()) / 1000.0)
-	p.currVideo.IsPlaying = true
-	fmt.Printf("%+v\n", p.message)
+	p.playerRepo.SetClock(p.roomID, p.clock)
 	return p.message, true
 }
 
 func (p playback) pause() (Message, bool) {
 	p.clock.SeekTo(p.timestamp).Pause()
 	fmt.Printf("Pause(), seconds elapsed: %2f\n", float64(p.clock.Elapsed()) / 1000.0)
-	p.currVideo.IsPlaying = false
+	p.playerRepo.SetClock(p.roomID, p.clock)
 	return p.message, true
 }
 
 func (p playback) seekTo() (Message, bool) {
 	p.clock.SeekTo(p.timestamp)
 	fmt.Printf("SeekTo(), seek to second: %2f\n", float64(p.clock.Elapsed()) / 1000.0)
+	p.playerRepo.SetClock(p.roomID, p.clock)
 	return p.message, true
 }
